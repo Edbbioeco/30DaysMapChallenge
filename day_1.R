a8c284e96128f4ac2a650f1c6c9f85e51b735c23
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-10-20 20:11:20 -0300

    Add files via upload

--- day_1.R
+++ day_1.R
@@ -61,10 +61,9 @@ baixar_correncias <- function(especies){
 
   ocorrencias <- dismo::gbif(genus = especies |> stringr::word(1),
                              species = especies,
-                             removeZeros = TRUE)
-
-  ocorrencias %<>%
-    dplyr::mutate(Espécie = especies)
+                             removeZeros = TRUE) |>
+    dplyr::mutate(Espécie = especies) |>
+    dplyr::select(lon, lat, Espécie)
 
   assign(paste0("ocorrencias_", especies |> stringr::word(2)),
          ocorrencias,
@@ -72,4 +71,120 @@ baixar_correncias <- function(especies){
 
 }
 
-purrr::map(especies, baixar_correncias)
+purrr::walk(especies, baixar_correncias)
+
+ls(pattern = "ocorrencias_") |>
+  mget(envir = globalenv())
+
+### Loop para exportar os dataframe ----
+
+exportar_ocorrencias <- function(especie, lista_correncias){
+
+  especie |>
+    crayon::green() |>
+    message()
+
+  lista_correncias |>
+  tidyr::drop_na() |>
+  dplyr::distinct(lon, lat, .keep_all = TRUE) |>
+    writexl::write_xlsx(paste0("ocorrencias_",
+                               especie |> stringr::word(2),
+                               ".xlsx"))
+
+}
+
+lista_correncias <- ls(pattern = "ocorrencias_") |>
+  mget(envir = globalenv())
+
+lista_correncias
+
+purrr::walk2(especies, lista_correncias, exportar_ocorrencias)
+
+### Reemportando ----
+
+importar_ocorrencias <- function(especie, caminho){
+
+  especie |>
+    crayon::green() |>
+    message()
+
+  df <- readxl::read_xlsx(caminho)
+
+  assign(paste0("ocorrencias_", especies |> stringr::word(2)),
+         df,
+         envir = globalenv())
+
+}
+
+caminho <- list.files(pattern = ".xlsx")
+
+purrr::walk2(especies, caminho, importar_ocorrencias)
+
+unido_ocorrencias <- ls(pattern = "ocorrencias_") |>
+  mget(envir = globalenv()) |>
+  dplyr::bind_rows()
+
+unido_ocorrencias
+
+### Visualizando ----
+
+ggplot() +
+  geom_sf(data = paises, color = "black", fill = "royalblue") +
+  geom_sf(data = america_latina, color = "black", fill = "gold") +
+  geom_point(data = unido_correncias, aes(lon, lat), size = 0.1) +
+  facet_wrap(~Espécie, ncol = 2)
+
+# Mapa ----
+
+## Criando o shapefile das ocorrências ----
+
+shp_ocorrencias <- unido_ocorrencias |>
+  tidyr::drop_na() |>
+  sf::st_as_sf(coords = c("lon", "lat"),
+               crs = america_latina |> sf::st_crs()) |>
+  sf::st_intersection(america_latina)
+
+shp_ocorrencias
+
+## Mapa ----
+
+ggplot() +
+  geom_sf(data = paises,
+          aes(fill = "North America"),
+          color = "black", linewidth = 0.5) +
+  geom_sf(data = america_latina,
+          aes(fill = "Latin America"),
+          color = "black", linewidth = 0.5) +
+  geom_sf(data = shp_ocorrencias,
+          aes(fill = "Occurence Point"),
+          color = "black",
+          shape = 21,
+          size = 0.75) +
+  coord_sf(xlim = c(-117.1278, -34.72999),
+           ylim = c(-55.61183, 32.72083),
+           label_graticule = "SWE") +
+  facet_wrap(~Espécie) +
+  scale_fill_manual(values = c("gray90",
+                               "forestgreen",
+                               "black"),
+                    breaks = c("North America",
+                               "Latin America",
+                               "Occurence Point")) +
+  scale_x_continuous(breaks = seq(-120, -34, 35)) +
+  scale_y_continuous(breaks = seq(-55, 30, 20)) +
+  labs(fill = NULL,
+       color = NULL,
+       title = "Caimaninae subfamily distribution along Latin America",
+       subtitle = "Source: Global Biodiversity Information Facility (GBIF)") +
+  theme_bw() +
+  theme(axis.text = element_text(size = 15, color = "black"),
+        legend.text = element_text(size = 15, color = "black"),
+        legend.position = "bottom",
+        strip.text = element_text(size = 15, color = "black", face = "italic"),
+        strip.background = element_rect(color = "black", linewidth = 1),
+        panel.border = element_rect(color = "black", linewidth = 1),
+        plot.title = element_text(size = 15, color = "black", hjust = 0.5),
+        plot.subtitle = element_text(size = 12.5, color = "black", hjust = 0.5)) +
+  ggview::canvas(height = 10, width = 12)
+
+ggsave(filename = "map_day1.png", height = 10, width = 12)
